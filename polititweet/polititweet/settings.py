93f6a5901427deab6f3a6e1e696ad15869da93a9
R. Miles McCain <x1-key@sendmiles.email>
2019-03-11 00:09:21 -0400

    Fix allowed hosts

--- polititweet/polititweet/settings.py
+++ polititweet/polititweet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 TWITTER_CREDENTIALS = {
     "consumer_key": os.environ.get("CONSUMER_KEY"),
