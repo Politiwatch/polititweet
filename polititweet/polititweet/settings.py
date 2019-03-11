bdca28b4394844a646ee992c561109b489dfca0a
R. Miles McCain <x1-key@sendmiles.email>
2019-03-10 22:33:18 -0400

    Add environment variables for database

--- polititweet/polititweet/settings.py
+++ polititweet/polititweet/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'polititweet.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'polititweet',
-        'USER': 'polititweet',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '',
+        'NAME': os.environ.get("db_name"),
+        'USER': os.environ.get("db_user"),
+        'PASSWORD': os.environ.get("db_password"),
+        'HOST': os.environ.get("db_host"),
+        'PORT': os.environ.get("db_port"),
     }
 }
 
