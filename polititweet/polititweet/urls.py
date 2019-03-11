8de51cc3dfb242211092326ccee09e73db33d0b1
R. Miles McCain <x1-key@sendmiles.email>
2019-03-11 19:07:27 -0400

    Add  tracking

--- polititweet/polititweet/urls.py
+++ polititweet/polititweet/urls.py
@@ -15,7 +15,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('', include('tracker.urls')),
 ]
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns = [
+#         path('__debug__/', include(debug_toolbar.urls)),
+
+#         # For django versions before 2.0:
+#         # url(r'^__debug__/', include(debug_toolbar.urls)),
+
+#     ] + urlpatterns
