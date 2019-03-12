e7fcbecfedf01436be12fbe5489e6246c426e078
R. Miles McCain <x1-key@sendmiles.email>
2019-03-11 22:09:19 -0400

    Update importer

--- polititweet/tracker/management/commands/import.py
+++ polititweet/tracker/management/commands/import.py
@@ -31,33 +31,39 @@ class Command(BaseCommand):
                     if skip:
                         break
                     if filename.endswith(".json"):
-                        with open(os.path.join(root, filename), "r") as infile:
-                            data = json.load(infile)
-                            if data["user"]["id"] in users_not_to_import:
-                                skip = True
-                                print("Skipping this user; already imported... (%s)" % str(data["user"]["id"]))
-                                break
-                            data["legacy_imported"] = True
-                            deleted = False
-                            if "deleted" in data and data["deleted"]:
-                                deleted = True
-                                user_deleted += 1
-                            if user == None or user.user_id != data["user"]["id"]:
-                                try:
-                                    user = User.objects.get(user_id=data["user"]["id"])
-                                    existing_ids = [tweet.tweet_id for tweet in Tweet.objects.filter(user=user)]
-                                except User.DoesNotExist:
-                                    self.stderr.write("Unable to find user %s in database... skipping..." % data["user"]["id"])
+                        try:
+                            with open(os.path.join(root, filename), "r") as infile:
+                                data = json.loads(infile.read().replace(u"\u0000", ""))
+                                if data["user"]["id"] in users_not_to_import:
                                     skip = True
-                                    continue
-                            tweet = Tweet(tweet_id=data["id"], full_data=data, user=user, deleted=deleted)
-                            tweet.full_text = tweet.text()[:300]
-                            if tweet.tweet_id not in existing_ids:
-                                to_batch_insert.append(tweet)
-                                total_imported += 1
-                                user_imported += 1
+                                    print("Skipping this user; already imported... (%s)" % str(data["user"]["id"]))
+                                    break
+                                data["legacy_imported"] = True
+                                deleted = False
+                                if "deleted" in data and data["deleted"]:
+                                    deleted = True
+                                    user_deleted += 1
+                                if user == None or user.user_id != data["user"]["id"]:
+                                    try:
+                                        user = User.objects.get(user_id=data["user"]["id"])
+                                        existing_ids = [tweet.tweet_id for tweet in Tweet.objects.filter(user=user)]
+                                    except User.DoesNotExist:
+                                        self.stderr.write("Unable to find user %s in database... skipping..." % data["user"]["id"])
+                                        skip = True
+                                        continue
+                                tweet = Tweet(tweet_id=data["id"], full_data=data, user=user, deleted=deleted)
+                                tweet.full_text = tweet.text()[:300]
+                                if tweet.tweet_id not in existing_ids:
+                                    to_batch_insert.append(tweet)
+                                    total_imported += 1
+                                    user_imported += 1
+                        except Exception as e:
+                            self.stderr.write("Encountered error: %s; continuing..." % str(e))
                 if not skip and user != None:
-                    Tweet.objects.bulk_create(to_batch_insert)
+                    try:
+                        Tweet.objects.bulk_create(to_batch_insert, 2500)
+                    except Exception as e:
+                        self.stderr.write("Encountered error while writing data: %s" % str(e))
                     user.deleted_count = Tweet.objects.filter(user=user, deleted=True).count()
                     user.save()
                     self.stdout.write(self.style.SUCCESS("Successfully imported %s tweets from @%s, of which %s were deleted." % (str(user_imported), user.full_data["screen_name"], str(user_deleted))))
