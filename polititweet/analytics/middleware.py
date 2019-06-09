008c878ded8ef3ec5dd323e9b70f2874a73e22fb
R. Miles McCain <x1-key@sendmiles.email>
2019-06-09 03:44:07 -0400

    Add analytics middleware

---
+++ polititweet/analytics/middleware.py
@@ -0,0 +1,18 @@
+from .models import Visit
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+class AnalyticsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        Visit.objects.create(ip=get_client_ip(request), url=request.build_absolute_uri())
+        return response
