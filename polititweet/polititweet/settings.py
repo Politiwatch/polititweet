ad200f5b0e09680b13f3f4f2663ae3bffdda8da6
R. Miles McCain <development-machine@sendmiles.email>
2020-08-08 20:41:24 +0000

    update kubernetes, db ssl

--- polititweet/polititweet/settings.py
+++ polititweet/polititweet/settings.py
@@ -101,6 +101,7 @@ DATABASES = {
         'PASSWORD': os.environ.get("DB_PASSWORD"),
         'HOST': os.environ.get("DB_HOST"),
         'PORT': os.environ.get("DB_PORT"),
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
